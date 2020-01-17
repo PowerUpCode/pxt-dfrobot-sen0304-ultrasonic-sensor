0b09766f500d89bd2a2507e1da9ce00a910f3980
PowerUpCode <59996579+PowerUpCode@users.noreply.github.com>
2020-01-17 20:45:15 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,24 @@
  * makecode PowerUpCode DFRobot IIC Ultrasonic Sensor package.
  */
 
-const APDS9930_I2C_ADDRESS = 0x39
-const APDS9930_CONTROL = 0x0F
+const SEN0304_I2C_ADDRESS = 0x11
+const MEASURE_MODE_PASSIVE = 0x00
+const MEASURE_RANG_500 = 0x20
+const CMD_DISTANCE_MEASURE = 0x01
+
+const CFG_INDEX = 0x07
+const CMD_INDEX = 0x08
+
+
+
+enum SEN0304_RANGE {
+    
+    RANGE_150 = 150,
+    RANGE_300 = 300,
+    RANGE_500 = 0x20
+}
+
+
 
 // AGAIN
 
@@ -31,20 +47,30 @@ namespace pxtsensor {
     let _AGAIN = 1;
     
     
-
+    /**
+     * setup Sensor
+     */
+    //% blockId="SEN0304_SET_RANGE" block="set Range %range"
+    //% weight=100 blockGap=8
+    function setup(range: SEN0304_RANGE): void {
+      let data =  (MEASURE_MODE_PASSIVE | range);//the measurement mode is set to passive mode, measurement range is set to 500CM.
+      i2cWriteBytes(CFG_INDEX, data);
+      de
+    }
+    
     /**
 
      * set APDS9930's reg
 
      */
 
-    function setReg(reg: number, dat: number): void {
+    function i2cWriteBytes(reg: number, dat: number): void {
 
         _wbuf[0] = reg | 0xA0;
 
         _wbuf[1] = dat;
 
-        pins.i2cWriteBuffer(APDS9930_I2C_ADDRESS, _wbuf);
+        pins.i2cWriteBuffer(SEN0304_I2C_ADDRESS, _wbuf);
 
     }
 
@@ -58,9 +84,9 @@ namespace pxtsensor {
 
     function getReg(reg: number): number {
 
-        pins.i2cWriteNumber(APDS9930_I2C_ADDRESS, reg, NumberFormat.UInt8BE);
+        pins.i2cWriteNumber(SEN0304_I2C_ADDRESS, reg, NumberFormat.UInt8BE);
 
-        return pins.i2cReadNumber(APDS9930_I2C_ADDRESS, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(SEN0304_I2C_ADDRESS, NumberFormat.UInt8BE);
 
     }    
     
