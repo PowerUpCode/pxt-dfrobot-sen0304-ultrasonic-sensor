a1c5a28f3f80049b0b195dbc0dfa3c6fd9794750
PowerUpCode <59996579+PowerUpCode@users.noreply.github.com>
2020-01-17 19:10:32 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,69 @@
 
 /**
- * makecode pxt-sensor IIC Proximity Sensor package.
+ * makecode pxtsensor IIC Proximity Sensor package.
  */
 
+const APDS9930_I2C_ADDRESS = 0x39
+const APDS9930_CONTROL = 0x0F
 
+// AGAIN
+
+enum APDS9930_AGAIN {
+
+    AGAIN_1 = 1,
+
+    AGAIN_8 = 8,
+
+    AGAIN_16 = 16,
+
+    AGAIN_120 = 120
+
+};
 /**
  * pxt-sensor mudule
  */
 //% weight=100 color=#102010 icon="\uf0eb" block="APDS9930"
-namespace pxt-sensor {
+namespace pxtsensor {
 
 
+    let _wbuf = pins.createBuffer(2);
+
+    let _AGAIN = 1;
+    
+    
+
+    /**
+
+     * set APDS9930's reg
+
+     */
+
+    function setReg(reg: number, dat: number): void {
+
+        _wbuf[0] = reg | 0xA0;
+
+        _wbuf[1] = dat;
+
+        pins.i2cWriteBuffer(APDS9930_I2C_ADDRESS, _wbuf);
+
+    }
+
+
+
+    /**
+
+     * get a reg
+
+     */
+
+    function getReg(reg: number): number {
+
+        pins.i2cWriteNumber(APDS9930_I2C_ADDRESS, reg, NumberFormat.UInt8BE);
+
+        return pins.i2cReadNumber(APDS9930_I2C_ADDRESS, NumberFormat.UInt8BE);
+
+    }    
+    
     /**
      * set ALS GAIN
      */
